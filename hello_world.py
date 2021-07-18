e206567087223bb5dc80e348b3fb6808a173b5e0
raghuayla <raghuvamshisten@gmail.com>
2021-07-19 00:01:11 +0530

    first commit

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+indian=["dal","naan","rotis"]
+chines=["egg rolls,fried rice,noodles"]
+italian=["cakes,pizzas,pasta"]
+dish=input("enter a dish name:")
+if dish  in indian:
+    print("indian")
+if dish is italian:
+    print("italian")
+if dish is chines:
+    print("chines")
+
+print("indian")
